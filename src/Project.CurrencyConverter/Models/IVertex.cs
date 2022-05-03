63924e9803a4a4d027a4f184739b601b7c97099e
Kamran Sadin <mrsadin@gmail.com>
2022-05-03 15:07:19 +0430

    Adding Vertext interface

---
+++ src/Project.CurrencyConverter/Models/IVertex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project.CurrencyConverter.Models
+{
+	public interface IVertex<T> : IComparable<IVertex<T>>, IEquatable<IVertex<T>>
+	{
+		//All Neighbors to select for shortest one
+		public List<IVertex<T>> Neighbors { get; }
+		public int Weight { get; set; }
+		public T Value { get; }
+        public IVertex<T> PreviousVertex { get; set; }
+        public object Data { get; set; }
+		public void AddNeighbor(IVertex<T> neighbor);
+    }
+}
+
